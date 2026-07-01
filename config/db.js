f34e8aae33e04dfa3e17bdf45e8648da90ce4c55
Ranim Jubaii <ranimjubaii53@gmail.com>
2026-07-01 17:07:10 +0300

    switch to postgres connection

--- config/db.js
+++ config/db.js
@@ -3,25 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('MySQL Database connected via Sequelize successfully.');
+    console.log('PostgreSQL Database connected via Sequelize successfully.');
     await sequelize.sync({ alter: false });
     console.log('Database tables synchronized.');
   } catch (error) {
-    console.error('Unable to connect to the MySQL database:', error.message);
+    console.error('Unable to connect to the PostgreSQL database:', error.message);
     process.exit(1);
   }
 };
